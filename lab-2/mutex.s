1ac7098f4a767e3c90c2cbff9c68c6c08ab633d2
Stanley Ding <stanleyding.tw@gmail.com>
2014-08-02 00:07:39 +0800

    Lab-2 finished.

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -9,7 +9,16 @@
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
-
+        push {R1, R2}
+        MOV R2, #locked
+loop:
+        LDREX R1, [R0]
+        CMP R1, #locked
+        BEQ loop
+        STREX R1, R2, [R0]
+        CMP R1, #0
+        BNE loop
+        POP {R1, R2}
         @ END CODE INSERT
 	bx lr
 
@@ -19,7 +28,10 @@ lock_mutex:
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
-        
+        PUSH {R1}
+        MOV R1, #unlocked
+        STR R1, [R0]
+        POP {R1}
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
